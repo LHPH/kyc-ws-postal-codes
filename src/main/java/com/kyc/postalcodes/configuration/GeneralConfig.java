d7d73cdfe0e01a8b842f0f52ae370476f555d308
LHPH <luislyly@hotmail.com>
2024-08-10 11:51:26 -0600

    Upgrade Spring Boot 3

--- src/main/java/com/kyc/postalcodes/configuration/GeneralConfig.java
+++ src/main/java/com/kyc/postalcodes/configuration/GeneralConfig.java
@@ -4,16 +4,12 @@ import com.kyc.core.config.LoadSimpleSqlConfig;
 import com.kyc.core.exception.handlers.KycGenericSoapExceptionHandler;
 import com.kyc.core.model.MessageData;
 import com.kyc.core.properties.KycMessages;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import jakarta.xml.bind.JAXBException;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
-
-import javax.xml.bind.JAXBException;
 
 import static com.kyc.postalcodes.constants.AppConstants.ERROR_CODE_001;
 
