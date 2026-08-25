1e923c25b17bc865291316b36df15a1f9364c9ba
LHPH <luislyly@hotmail.com>
2026-08-25 17:41:01 -0600

    Adjust due latest changes for SOAP exception handling in kyc-core

--- src/main/java/com/kyc/postalcodes/configuration/GeneralConfig.java
+++ src/main/java/com/kyc/postalcodes/configuration/GeneralConfig.java
@@ -4,17 +4,17 @@ import com.kyc.core.config.LoadSimpleSqlConfig;
 import com.kyc.core.exception.handlers.KycGenericSoapExceptionHandler;
 import com.kyc.core.model.MessageData;
 import com.kyc.core.properties.KycMessages;
-import jakarta.xml.bind.JAXBException;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-import static com.kyc.postalcodes.constants.AppConstants.ERROR_CODE_001;
-
 @Configuration
-@Import(value = {LoadSimpleSqlConfig.class, KycMessages.class})
+@Import(value = {
+        LoadSimpleSqlConfig.class,
+        KycMessages.class
+})
 @EnableCaching
 public class GeneralConfig {
 
@@ -27,8 +27,8 @@ public class GeneralConfig {
     }
 
     @Bean
-    public KycGenericSoapExceptionHandler kycGenericSoapExceptionHandler(KycMessages kycMessages) throws JAXBException {
+    public KycGenericSoapExceptionHandler kycGenericSoapExceptionHandler(KycMessages kycMessages) {
 
-        return new KycGenericSoapExceptionHandler(kycMessages.getMessage(ERROR_CODE_001), marshaller());
+        return new KycGenericSoapExceptionHandler(kycMessages, marshaller());
     }
 }
